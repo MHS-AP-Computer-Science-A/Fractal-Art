7d2ab2bc171b722728863fd0f4a724516b794060
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2023-05-05 09:30:56 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-       FractalStarter b = new FractalStarter(4);
+       new BoxFractal(4);
     	
     }
 }
